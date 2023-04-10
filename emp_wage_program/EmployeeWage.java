f5f23db0afa2ad735e7210d98fca6c0fa6b7a7d0
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-04-10 23:36:41 +0530

    UC4-Solving using Switch Case Statement

--- emp_wage_program/EmployeeWage.java
+++ emp_wage_program/EmployeeWage.java
@@ -1,20 +1,23 @@
 package emp_wage_program;
 
 public class EmployeeWage {
-	static final int IS_FULL_TIME = 1;
-	static final int IS_PART_TIME = 2;
-	static final int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
 		int empHrs = 0;
 		int empWage = 0;
 
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-		} else if (empCheck == IS_PART_TIME) {
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case IS_PART_TIME:
 			empHrs = 4;
-		} else {
+			break;
+		case IS_FULL_TIME:
+			empHrs = 8;
+			break;
+		default:
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
