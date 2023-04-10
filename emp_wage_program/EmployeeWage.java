1a96766ea8a39b16684fe029d93ae226937c3818
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-04-10 22:26:26 +0530

    UC2-Calculate_Daily_emp_wage

--- emp_wage_program/EmployeeWage.java
+++ emp_wage_program/EmployeeWage.java
@@ -1,16 +1,21 @@
 package emp_wage_program;
 
 public class EmployeeWage {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-		int IS_FULL_TIME = 1;
-		int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-		System.out.println(empCheck);
-		if (empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is present");
-		} else {
-			System.out.println("Employee is absent");
-		}
-	}
+	static final int IS_FULL_TIME = 1;
+	static final int EMP_RATE_PER_HOUR = 20;
 
+	public static void main(String[] args) {
+		int empHrs = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+
+		if (empCheck == IS_FULL_TIME) {
+			empHrs = 8;
+		} else {
+			empHrs = 0;
+		}
+		empWage = empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage :  " + empWage);
+
+	}
 }
